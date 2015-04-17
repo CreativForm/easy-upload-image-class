277d73ea9c7ab046b1c69535dd81001e11ef288a
CreativForm <creativform@gmail.com>
2015-04-17 16:51:14 +0200

    New update on version 2.0
    I removed the version 1.x and add new version 2.x.
    All options remain the same but core inside is very improved and optimized.

--- core_image_upload.php
+++ core_image_upload.php
@@ -1,6 +1,6 @@
 <?php
 /**********************************************************
- * EASY UPLOAD IMAGE CLASS v1.0.6 (http://creativform.com)
+ * EASY UPLOAD IMAGE CLASS v2.0 (http://creativform.com)
  * created: 12/25/2014 12:00 (EST)
  * Copyright 2014 CreativForm.com
  * Created by: Ivijan-Stefan Stipic (creativform@gmail.com)
@@ -56,25 +56,43 @@
 */
 class uploadImage
 {
-	public $size		=	array();
-	public $extension	=	array();
+	/* Public Methods */
 	public $results		=	array(
 		'return'=>false,
 		'message'=>NULL
 	);
-	protected $option	=	array();
-	protected $ext		=	false;
-	protected $file		=	false;
-	protected $post_errors= array(
-        0=>"There is no error, the file uploaded with success",
-        1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini",
-        2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
-        3=>"The uploaded file was only partially uploaded",
-        4=>"No file was uploaded",
-        6=>"Missing a temporary folder"
-	); 
-	function __construct($options=array())
-	{
+	
+	/* Protected Methods */
+	private $option	=	array();
+	private $ext		=	false;
+	private $files	=	false;
+	private $alert = array(
+        0	=>	"Image is uploaded successfully!",
+		1	=>	"All images is successfully uploaded!",
+		2	=>	"Appeared an unexpected error!",
+        3	=>	"The uploaded file exceeds the upload_max_filesize directive in php.ini!",
+        4	=>	"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form!",
+        5	=>	"The uploaded file was only partially uploaded!",
+        6	=>	"No file was uploaded!",
+        7	=>	"Temporary folder missing!",
+		8	=>	"Invalid file type. Allowed files types are: {FILE-TYPE}!",
+		9	=>	"Imag what you want to upload is larger ({IMG-SIZE}) than allowed size: {ALLOWED-SIZE}!",
+		10	=>	"{IMAGE-NAME} already exists.",
+		11	=>	"Image what you want to upload is not between {MIN-SIZE} and {MAX-SIZE}!",
+	);
+	private $allowedExtensions=array('jpeg','jpg','gif','png','bmp');
+	
+	function __construct($options=array()){
+		// Set options
+		$this->option=$this->_setup($options);
+		// Setup $_FILES
+		$this->files =  $_FILES[$this->option['input_name']];
+		$this->results=$this->_upload();
+	}
+
+	// Replace default with custom options and setup all
+	private function _setup($options){
+		/* Default Options */
 		$option=array(
 			// Rename image
 			'new_name'		=>	NULL,
@@ -101,180 +119,207 @@ class uploadImage
 			// image quality in % percentages min=0, max=100 (have overload protection)
 			'quality'		=>	100,
 		);
-		// manual change values
-		foreach($options as $key=>$value)
-		{
-			if(!empty($key))
-			{
-				unset($option[$key]);
-				$option[$key]=$value;
-			}
+		/* Setup Options */
+		if(function_exists("array_replace") && version_compare(phpversion(), '5.3.0', '>='))
+			$option = array_replace($option, $options); // (PHP 5 >= 5.3.0)
+		else 
+			$option = array_merge($option, $options); // (PHP 5 < 5.3.0)
+		/* Fix some options */
+		$extensions	=	(is_array($option['extensions'])?$option['extensions']:array_map("trim",explode(",",$option['extensions'])));
+		$new_name	=	$this->_cleanName($_FILES[$option['input_name']]['name'],$option['hash'],$option['new_name']);
+		/* Setup folder if not exists */
+		$this->_mkDirectory($option);
+		/* Merge and return all */
+		if(function_exists("array_replace") && version_compare(phpversion(), '5.3.0', '>='))
+			return array_replace($option, array(
+				'extensions'	=>	$extensions,
+				'new_name'		=>	$new_name
+			)); // (PHP 5 >= 5.3.0)
+		else 
+			return array_merge($option, array(
+				'extensions'	=>	$extensions,
+				'new_name'		=>	$new_name
+			)); // (PHP 5 < 5.3.0)
+	}
+	
+	// Get extension of file
+	private function _checkExtension($name,$type){
+		// Get extension
+		$ext	=	explode(".",$name);
+		$ext	=	end($ext);
+		$ext	=	strtolower($ext);
+		$ext	=	trim($ext);
+		// get options
+		$allowedExt=(is_array($this->option['extensions'])?
+						$this->option['extensions']:array_map("trim",explode(",",$this->option['extensions'])));
+		// check mime type
+		$mime=array();
+		foreach($allowedExt as $e){
+			if(in_array($e, $this->allowedExtensions)) $mime[]="image/".$e;
 		}
-		if(isset($_FILES[$option['input_name']]["name"]) && !empty($_FILES[$option['input_name']]["name"]))
-		{
-			// if file have error
-			if ($_FILES[$option['input_name']]["error"] > 0)
-			{
-				$this->results = array(
-					'return'=>false,
-					'message'=>$this->post_errors[$_FILES[$option['input_name']]['error']],
-				);
-			}
-			else
-			{
-				// Private
-				$this->file			=	$_FILES[$option['input_name']];
-				$this->option		=	$option;
-				@$this->ext			=	strtolower(end(explode(".",implode("",explode("\\",$_FILES[$option['input_name']]['name'])))));
-				$this->mk_directory();
-				// Public
-				$this->size			=	$this->size();
-				$this->extension	=	$this->extension();
-				// make directory if not exists
-				// Functions
-				$this->results 		= $this->upload();
-			}
-		}
-		else
-		{
-			$this->results = array(
-				'return'=>false,
-				'message'=>"No file selected. Please select file for upload!",
-			);	
+		// return
+		if((count($mime) > 0) && in_array($type, $mime, true) && in_array($ext, $allowedExt, true) ){
+			return (object) array(
+				'return'	=>	true,
+				'extension'	=>	$ext,
+				'type'		=>	$type,
+				'message'	=>	NULL
+			);
+		}else{
+			$message=str_replace("{FILE-TYPE}",$this->option['extensions'],$this->alert[8]);
+			return (object) array(
+				'return'	=>	false,
+				'message'	=>	$message,
+			);
 		}
 	}
-	/* 
-		Check image size 
-		$this->size();
-		return array();
-	*/
-	protected function size()
-	{
+	
+	// Clean and hash image name
+	private function _cleanName($str,$hash,$new_name=""){
+		if(!empty($new_name) && $new_name!==''){
+			$str=$new_name;
+		}
+		$exist = array('/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.'|[\x00-\x7F][\x80-\xBF]+'.'|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.'|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.'|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S','/\xE0[\x80-\x9F][\x80-\xBF]'.'|\xED[\xA0-\xBF][\x80-\xBF]/S','/\%/','/\@/','/\&/','/\s[\s]+/','/[\s\W]+/','/^[\-]+/','/[\-]+$/');
+		$replace = array('','','%',' at ',' and ','-','-','','');
+		$name = preg_replace($exist,$replace,$str);
+		$name = strtolower(trim($name));
+		$name = str_replace(array(' ','\r'),'-',$name);
+		return ((bool)$hash===true ? md5($name) : $name);
+	}
+	
+	// setup directory
+	private function _mkDirectory($option){
+		// make directory in not exist
+		if (!file_exists($option['location']) && !is_dir($option['location'])){
+			mkdir($option['location'], 0766, true);
+		}
+		// make empty index.htm if not exist
+		if (!file_exists($option['location'].'index.htm') && !is_file($option['location'].'index.htm')){
+			$file_html = @fopen($option['location'].'index.htm','w');
+			fclose($file_html);
+		}
+	}
+	
+	// Check image size
+	private function _checkSize($_fileSize, $_tmp_name){
+		list($width,$height)=getimagesize($_tmp_name);
 		if(round($this->option['min_width']*$this->option['min_height']) > 0)
 		{
 			if(
 				(round($this->option['max_width']*$this->option['max_height']) > 0) && 
-				($this->file["size"] <= round($this->option['max_width']*$this->option['max_height'])) &&
-				($this->file["size"] >= round($this->option['min_width']*$this->option['min_height']))
-			)
-			{
+				($_fileSize <= round($this->option['max_width']*$this->option['max_height'])) &&
+				($_fileSize >= round($this->option['min_width']*$this->option['min_height']))
+			){
 				// Size is OK
-				return array(
+				return (object) array(
 					'return'	=>	true,
+					'width'		=>	$width,
+					'height'	=>	$height,
+					'message'	=>	NULL,
+				);	
+			}else{
+				$message=str_replace(
+							array('{MIN-SIZE}','{MAX-SIZE}'),
+							array(
+								$this->option['min_width']."x".$this->option['min_height'],
+								$this->option['max_width'].'x'.$this->option['max_height']
+							),
+							$this->alert[11]
+						);
+				return (object) array(
+					'return'	=>	false,
+					'width'		=>	$width,
+					'height'	=>	$height,
+					'message'	=>	$message,
+				);
+			}
+		}
+		else
+		{
+			if((round($this->option['max_width']*$this->option['max_height']) > 0)){
+				if(($_fileSize <= round($this->option['max_width']*$this->option['max_height']))){
+					// Size is OK
+					return (object) array(
+						'return'	=>	true,
+						'width'		=>	$width,
+						'height'	=>	$height,
+						'message'	=>	NULL,
+					);	
+				}else{
+					$message=str_replace(
+								array('{MIN-SIZE}','{MAX-SIZE}'),
+								array($width."x".$height,$this->option['max_width'].'x'.$this->option['max_height']),
+								$this->alert[11]
+							);
+					return (object) array(
+						'return'	=>	false,
+						'width'		=>	$width,
+						'height'	=>	$height,
+						'message'	=>	$message,
+					);
+				}
+			}else{
+				// unlimited image size
+				return (object) array(
+					'return'	=>	true,
+					'width'		=>	$width,
+					'height'	=>	$height,
 					'message'	=>	NULL,
 				);	
 			}
-			else
-			{
-				// Too large image
-				list($width,$height)=getimagesize($this->file["tmp_name"]);
-				return array(
-					'return'	=>	false,
-					'message'	=>	"Imag what you want to upload is not between ".
-									$this->option['max_width'].'x'.$this->option['max_height'].'px and '.$this->option['min_width'].'x'.$this->option['min_height'].'px',
-				);	
-			}
-		}
-		else
-		{
-			if((round($this->option['max_width']*$this->option['max_height']) > 0))
-			{
-				if(($this->file["size"] <= round($this->option['max_width']*$this->option['max_height'])))
-				{
-					// Size is OK
-					return array(
-						'return'	=>	true,
-						'message'	=>	NULL,
-					);	
-				}
-				else
-				{
-					// Too large image
-					list($width,$height)=getimagesize($this->file["tmp_name"]);
-					return array(
-						'return'	=>	false,
-						'message'	=>	"Imag what you want to upload is larger (".$width."x".$height.
-										") than allowed size: ".$this->option['max_width'].'x'.$this->option['max_height'].'px',
-					);	
-				}
-			}
-			else
-			{
-				// unlimited image size
-				return array(
-						'return'	=>	true,
-						'message'	=>	NULL,
-					);	
-			}
 		}
 	}
-	/* 
-		Check image Extension 
-		$this->extension();
-		return array();
-	*/
-	protected function extension()
-	{
-		$extensions=(is_array($this->option['extensions'])?$this->option['extensions']:array_map("trim",explode(",",$this->option['extensions'])));
-		$type=array();
-		foreach($extensions as $e)
-		{
-			if(in_array($e, array('jpeg','jpg','gif','png','bmp')))
-			{
-				$type[]="image/".$e;
-			}
-		}
-		if((count($type) > 0) && in_array($this->ext, $extensions, true) && in_array($this->file['type'], $type, true) )
-		{
-			return array(
-				'return'=>true,
-				'message'=>NULL
-			);
-		}
-		else
-		{
-			return array(
-				'return'=>false,
-				'message'=>"Invalid file type. Allowed files types are: ".$this->option['extensions'],
-			);
-		}
+	
+	/* Upload image */
+	private function _upload(){
+		$return=$this->_imgUpload($this->files);
+		return array(
+			'return'	=>	$return->return,
+			'message'	=>	$return->message,
+			'name'		=>	$return->name,
+			'location'	=>	$return->location,
+			'path'		=>	$return->path,
+			'quality'	=>	$return->quality,
+			'extension'	=>	$return->extension,
+			'width'		=>	$return->width,
+			'height'	=>	$return->height,
+		);
 	}
-	/* 
-		Upload and resize image 
-		$this->upload();
-		return array();
-	*/
-	protected function upload()
-	{
-		if($this->size['return']===true)
-		{
-			if($this->extension['return']===true)
-			{
-				/* Setup images */
-				$file_name=preg_replace("/[^a-zA-Z0-9\.\-\_\%\$\ ]/","",(!empty($this->option['new_name'])?$this->option['new_name']:$this->file['name']));
-				$image=$this->file['tmp_name'];
-				switch ($this->ext)
+	
+	// Recalculate and upload new image
+	private function _imgUpload($POST_FILES){
+		// check size
+	//	var_dump($POST_FILES);
+		$size=$this->_checkSize($POST_FILES['size'], $POST_FILES['tmp_name']);
+		if((bool)$size->return === true){
+			// check extension
+			$extension=$this->_checkExtension($POST_FILES['name'], $POST_FILES['type']);
+			if((bool)$extension->return === true){
+				// get image
+				$image=$POST_FILES['tmp_name'];
+				// encode image
+				switch ($extension->extension)
 				{
 					case "png":
-						$src = @imagecreatefrompng($image);
+						$src = imagecreatefrompng($image);
 						$background = imagecolorallocate($src, 0, 0, 0);
-						@imagecolortransparent($src, $background);
-						@imagealphablending($src, false);
-						@imagesavealpha($src, true);
+						imagecolortransparent($src, $background);
+						imagealphablending($src, false);
+						imagesavealpha($src, true);
 					break;
 					case "gif":
-						$src = @imagecreatefromgif($image);
+						$src = imagecreatefromgif($image);
 						$background = imagecolorallocate($src, 0, 0, 0);
-						@imagecolortransparent($src, $background);
+						imagecolortransparent($src, $background);
 					break;
 					case "bmp":
-						$src = @imagecreatefromwbmp($image);
+						$src = imagecreatefromwbmp($image);
 					break;
 					default:
-						$src = @imagecreatefromjpeg($image);
+						$src = imagecreatefromjpeg($image);
 					break;
 				}
-				/* Resize */
+				// Recalculate and resize
 				list($width,$height)=getimagesize($image);
 				if($this->option['new_width'] > 0 || $this->option['new_height'] > 0)
 				{
@@ -297,129 +342,75 @@ class uploadImage
 				}
 				// recalculate and create new resized image
 				$tmp=imagecreatetruecolor($newWidth,$newHeight);
-				switch ($this->ext)
+				switch ($extension->extension)
 				{
 					case "png":
-						@imagealphablending($tmp, FALSE);
-						@imagesavealpha($tmp, TRUE);
-						@imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
+						imagealphablending($tmp, false);
+						imagesavealpha($tmp, true);
+						imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
 						$background = imagecolorallocate($tmp, 0, 0, 0); 
-						@imagecolortransparent($tmp, $background);
+						imagecolortransparent($tmp, $background);
 					break;
 					case "gif":
-						@imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
+						imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
 						$background = imagecolorallocate($tmp, 0, 0, 0); 
-						@imagecolortransparent($tmp, $background);
+						imagecolortransparent($tmp, $background);
 					break;
 					default:
-						@imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
+						imagecopyresampled($tmp,$src,0,0,0,0,$newWidth,$newHeight,$width,$height);
 					break;
 				}
-				// rename
-				@$rename=(empty($this->option['new_name'])?implode("",explode('.'.$this->ext,strtolower($this->file["name"]))):$this->option['new_name']);
-				$rename=strtolower($rename);
-				if(is_bool($this->option['hash']) && $this->option['hash']===true)
-				{
-					$newName=$this->encode($rename).'.'.$this->ext;
-				}
-				else
-				{
-					$newName=$this->set_name($rename).'.'.$this->ext;
-				}
-				$location = NULL;
-				if (file_exists($this->option['location'].$newName))
-				{
-					@imagedestroy($src);
-					@imagedestroy($tmp);
-					return array(
-						'return'=>false,
-						'message'=>$newName . " already exists. ",
-					);
-				}
-				else
-				{
-					// store image on server
-					$location = $this->option['location'].$newName; 
-				}
-				switch ($this->ext)
+				// Setup Name
+				$newName=$this->option['new_name'].'.'.$extension->extension;
+				$location = $this->option['location'].$newName;
+				switch ($extension->extension)
 				{
 					case "png":
 						$quality = round(9 - $this->option['quality'] / 11.11);
-						@imagepng($tmp,$location,$quality);
+						imagepng($tmp,$location,$quality);
 					break;
 					case "gif":
-						@imagegif($tmp,$location);
+						$quality=NULL;
+						imagegif($tmp,$location);
 					break;
 					case "bmp":
-						@imagewbmp($tmp,$location);
+						$quality=NULL;
+						imagewbmp($tmp,$location);
 					break;
 					default:
 						$quality=(($this->option['quality']>=100) ? 100 : ($this->option['quality']<=0 ? 0 : $this->option['quality']));
-						@imagejpeg($tmp,$location,$quality);
+						imagejpeg($tmp,$location,$quality);
 					break;
 				}
-				@imagedestroy($src);
-				@imagedestroy($tmp);
-				return array(
+				imagedestroy($src);
+				imagedestroy($tmp);
+				return (object) array(
 					'return'	=>	true,
-					'message'	=>	"You successfuly upload image!",
+					'message'	=>	$this->alert[0],
 					'name'		=>	$newName,
 					'location'	=>	$location,
+					'path'		=>	$location.$newName,
 					'quality'	=>	$quality,
-					'extension'	=>	$this->ext,
+					'extension'	=>	$extension->extension,
 					'width'		=>	$newWidth,
 					'height'	=>	$newHeight,
 				);
 			}
-			else
-			{
-				return $this->extension['message'];
+			else return $extension->message;
+		}
+		else return $size->message;
+	}
+	
+	private function _reArrayFiles(&$file_post) {
+		$file_ary = array();
+		$file_count = count($file_post['name']);
+		$file_keys = array_keys($file_post);
+		for ($i=0; $i<$file_count; $i++) {
+			foreach ($file_keys as $key) {
+				$file_ary[$i][$key] = $file_post[$key][$i];
 			}
 		}
-		else
-		{
-			return $this->size['message'];
-		}
-	}
-	## clean filename or generate hash name
-	protected function set_name($content)
-	{
-		$exist = array('/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.'|[\x00-\x7F][\x80-\xBF]+'.'|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.'|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.'|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S','/\xE0[\x80-\x9F][\x80-\xBF]'.'|\xED[\xA0-\xBF][\x80-\xBF]/S','/\%/','/\@/','/\&/','/\s[\s]+/','/[\s\W]+/','/^[\-]+/','/[\-]+$/');
-		$replace = array('','',' % ',' at ',' and ','-','-','','');
-		$content = preg_replace($exist,$replace,$content);
-		return strtolower(trim($content));
-	}
-	## setup directory
-	private function mk_directory()
-	{
-		// make directory in not exist
-		if (!file_exists($this->option['location']) && !is_dir($this->option['location'])) {
-			@mkdir($this->option['location'], 0766, true);
-		}
-		// make empty index.htm if not exist
-		if (!file_exists($this->option['location'].'index.htm') && !is_file($this->option['location'].'index.htm')) {
-			$file_html = @fopen($this->option['location'].'index.htm','w');
-			@fclose($file_html);
-		}
-	}
-	## encode image name
-	protected function encode($string)
-	{
-		return strtolower(
-				implode("",
-					explode("=",
-						str_rot13(
-							base64_encode(
-								str_rot13(
-									base64_encode(
-										$this->set_name($string)
-										)
-									)
-								)
-							)
-						)
-					)
-				);
+		return $file_ary;
 	}
 }
 ?>
